23e87a8f1b24dbf08fb578ab4f6ca623feab0dc3
deku <kesavansivakumar50@gmail.com>
2026-07-09 22:37:28 +0530

    Added missing imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
-import About from "@/components/About";
-import Portfolio from "@/components/Portfolio";
+import About from "@/components/About-temp";
+import Portfolio from "@/components/Portfolio-temp";
 import BudgetPlanner from "@/components/BudgetPlanner";
-import Testimonials from "@/components/Testimonials";
-import Partners from "@/components/Partners";
+import Testimonials from "@/components/Testimonials-temp";
+import Partners from "@/components/Partners-temp";
 import Footer from "@/components/Footer";
 
 export default function Home() {
